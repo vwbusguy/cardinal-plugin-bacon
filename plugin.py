58cf29c0e15a49ea85d149f5d5da1826ff327e4d
Scott A. Williams <vwbusguy@fedoraproject.org>
2021-10-28 11:51:18 -0700

    Ignore zodbot

--- plugin.py
+++ plugin.py
@@ -10,14 +10,15 @@ class BaconPlugin(object):
     @help("Syntax: bacon")
     def bacon(self, cardinal, user, channel, msg):
         nick = user.nick
-        cardinal.sendMsg(channel, self.get_bacon_msg(nick))
+        if "zodbot" not in nick: 
+            cardinal.sendMsg(channel, self.get_bacon_msg(nick))
 
     def get_bacon_msg(self,nick):
         messages = ("⌇⌇⌇ Bacon Party ⌇⌇⌇", 
                 "Welcome to the bacon party, %s!" % nick,
                 "Bacon is a vegetable.",
                 "You know, it's hard to beat bacon at anytime of day. — Nick Offerman",
-                '''I'm not sure how healthy bacon is in general, but I know it's incredibly delicious." — Gwyneth Paltrow''',
+                '''"I'm not sure how healthy bacon is in general, but I know it's incredibly delicious." — Gwyneth Paltrow''',
                 '''"I unfortunately still crave chicken McNuggets and bacon, which is the meat candy of the world." — Katy Perry''',
                 '''"Who cannot appreciate the smell of bacon?" — Tony Cardenas''',
                 'GPL is great and all, %s, but have you tried bacon?' % nick,

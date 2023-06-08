48f551a70013b41f8e293039b72a561653866555
Scott A. Williams <vwbusguy@fedoraproject.org>
2023-06-08 09:27:57 -0700

    Respond to bacon emoji

--- plugin.py
+++ plugin.py
@@ -5,7 +5,7 @@ class BaconPlugin(object):
     def __init__(self, cardinal, config):
         pass
 
-    @regex(r'[Bb8][Aa4][Cc][Oo0][Nn](?![Bb][oO][tT])')
+    @regex(r'([Bb8][Aa4][Cc][Oo0][Nn](?![Bb][oO][tT])|🥓)')
     @help("Talk about bacon.")
     @help("Syntax: bacon")
     def bacon(self, cardinal, user, channel, msg):
